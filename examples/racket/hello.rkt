b641d5c1be4c1723245b672b8d43ba7c2bcab3a5
Sean Buttinger <sean@antfarm.de>
2026-02-09 11:53:05 +0100

    Refactored example programs.

--- examples/racket/hello.rkt
+++ examples/racket/hello.rkt
@@ -3,32 +3,40 @@
 (require gridcode/grid/main
          gridcode/gridcode)
 
+(define display-name "Hello")
+(define grid-size 16)
+(define frame-rate 2)
+
+(define (setup-grid)
+  (for ([x (in-range 16)])
+    (set-cell! x x "alive" #t)))
+
+(define (update-grid)
+  (for* ([x (in-range 16)]
+         [y (in-range 16)])
+    (define alive (get-cell x y "alive"))
+    (set-cell! x y "alive" (not alive))))
+
+(define (color-for-cell x y)
+  (if (get-cell x y "alive")
+      (color 1.0 0.8 0.2)
+      (color 0.0 0.0 0.0)))
+
+(define (info-for-cell x y)
+  (format "[~a|~a] alive: ~a" x y (get-cell x y "alive")))
+
+(define (handle-cell-tapped x y)
+  (define alive (get-cell x y "alive"))
+  (set-cell! x y "alive" (not alive)))
+
 (define program
-  (hash 'display-name "Hello"
-
-        'grid-size 16
-        'frame-rate 2
-
-        'setup-grid (lambda ()
-                      (for ([x (in-range 16)])
-                        (set-cell! x x "alive" #t)))
-
-        'update-grid (lambda ()
-                       (for* ([x (in-range 16)]
-                              [y (in-range 16)])
-                         (define alive (get-cell x y "alive"))
-                         (set-cell! x y "alive" (not alive))))
-
-        'color-for-cell (lambda (x y)
-                          (if (get-cell x y "alive")
-                              (color 1.0 0.8 0.2)
-                              (color 0.0 0.0 0.0)))
-
-        'info-for-cell (lambda (x y)
-                         (format "[~a|~a] alive: ~a" x y (get-cell x y "alive")))
-
-        'handle-cell-tapped (lambda (x y)
-                              (define alive (get-cell x y "alive"))
-                              (set-cell! x y "alive" (not alive)))))
+  (hash 'display-name display-name
+        'grid-size grid-size
+        'frame-rate frame-rate
+        'setup-grid setup-grid
+        'update-grid update-grid
+        'color-for-cell color-for-cell
+        'info-for-cell info-for-cell
+        'handle-cell-tapped handle-cell-tapped))
 
 (run program)
