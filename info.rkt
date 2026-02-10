ce0dbd8dc3a8888a394e24ea6b33dd08834526c1
Sean Buttinger <sean@antfarm.de>
2026-02-10 20:48:09 +0100

    Added launcher.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,11 @@
 #lang info
 
 (define collection "gridcode")
+
 (define deps '("base" "gui-lib"))
+
+(define racket-launcher-names '("gridcode"))
+(define racket-launcher-libraries '("gridcode.rkt"))
 
 ;; To re-install:
 ;;   raco pkg remove gridcode
